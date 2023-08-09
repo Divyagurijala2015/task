5e274520df8e350ddbc82e284cddf0dcf55af123
Your Name <you@example.com>
2023-08-09 15:51:03 +0530

    changes

---
+++ task.js
@@ -0,0 +1,13 @@
+// script.js
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    cards.forEach(otherCard => {
+      if (otherCard !== card) {
+        otherCard.classList.remove("expanded");
+      }
+    });
+    card.classList.toggle("expanded");
+  });
+});
